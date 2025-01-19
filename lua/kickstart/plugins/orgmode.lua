699ee58636717e1b4cd3086fbb030e8375e28018
Leoo0x1 <leonardomarchi14@gmail.com>
2025-01-19 16:23:30 +0100

    orgmode configuration

---
+++ lua/kickstart/plugins/orgmode.lua
@@ -0,0 +1,27 @@
+return {
+  'nvim-orgmode/orgmode',
+  event = 'VeryLazy',
+  ft = { 'org' },
+  config = function()
+    -- Setup orgmode
+    require('orgmode').setup {
+      org_agenda_files = '~/Org/**/*',
+      org_default_notes_file = '~/Org/refile.org',
+    }
+
+    require('cmp').setup {
+      sources = {
+        { name = 'orgmode' },
+      },
+    }
+    vim.opt.conceallevel = 2
+    vim.opt.concealcursor = 'nc'
+
+    -- NOTE: If you are using nvim-treesitter with ~ensure_installed = "all"~ option
+    -- add ~org~ to ignore_install
+    -- require('nvim-treesitter.configs').setup({
+    --   ensure_installed = 'all',
+    --   ignore_install = { 'org' },
+    -- })
+  end,
+}
