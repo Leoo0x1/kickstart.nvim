de77a4016df0a8c73ef8c88c20abc2deba3efb9e
Leoo0x1 <leonardomarchi14@gmail.com>
2025-02-03 09:07:48 +0100

    added nodejs setup

--- init.lua
+++ init.lua
@@ -618,6 +618,7 @@ require('lazy').setup({
       local servers = {
         -- clangd = {},
         gopls = {},
+        ts_ls = {},
         -- pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -23,6 +23,11 @@ return {
 
     -- Add your own debuggers here
     'leoluz/nvim-dap-go',
+    'mxsdev/nvim-dap-vscode-js',
+    {
+      'microsoft/vscode-js-debug',
+      build = 'npm install --legacy-peer-deps && npx gulp vsDebugServerBundle && mv dist out',
+    },
   },
   keys = {
     -- Basic debugging keymaps, feel free to change to your liking!
@@ -96,6 +101,7 @@ return {
         -- Update this to ensure that you have the debuggers for the langs you want
         'delve',
         'codelldb',
+        'js',
       },
     }
 
@@ -144,5 +150,42 @@ return {
     --   },
     -- }
     require('dap-go').setup()
+    require('dap-vscode-js').setup {
+      -- node_path = 'node', -- Path of node executable. Defaults to $NODE_PATH, and then "node"
+      debugger_path = '/home/leo/.local/share/nvim/lazy/vscode-js-debug', -- Path to vscode-js-debug installation.
+      -- debugger_cmd = { '/home/leo/.local/share/nvim/mason/bin/js-debug-adapter' }, -- Command to use to launch the debug server. Takes precedence over `node_path` and `debugger_path`.
+      adapters = { 'pwa-node', 'pwa-chrome', 'pwa-msedge', 'node-terminal', 'pwa-extensionHost' }, -- which adapters to register in nvim-dap
+      -- log_file_path = '/home/leo/.cache/nvim/dap_vscode_js.log', -- Path for file logging
+      -- log_file_level = 2, -- Logging level for output to file. Set to false to disable file logging.
+      -- log_console_level = vim.log.levels.ERROR, -- Logging level for output to console. Set to false to disable console output.
+    }
+    local js_based_languages = { 'typescript', 'javascript', 'typescriptreact' }
+
+    for _, language in ipairs(js_based_languages) do
+      require('dap').configurations[language] = {
+        {
+          type = 'pwa-node',
+          request = 'launch',
+          name = 'Launch file',
+          program = '${file}',
+          cwd = '${workspaceFolder}',
+        },
+        {
+          type = 'pwa-node',
+          request = 'attach',
+          name = 'Attach',
+          processId = require('dap.utils').pick_process,
+          cwd = '${workspaceFolder}',
+        },
+        {
+          type = 'pwa-chrome',
+          request = 'launch',
+          name = 'Start Chrome with "localhost"',
+          url = 'http://localhost:3000',
+          webRoot = '${workspaceFolder}',
+          userDataDir = '${workspaceFolder}/.vscode/vscode-chrome-debug-userdatadir',
+        },
+      }
+    end
   end,
 }
